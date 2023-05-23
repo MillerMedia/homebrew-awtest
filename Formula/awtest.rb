96fa035fd24575fcbe1ae856af4571eb486fcf77
Matt Miller <matt@miller-media.com>
2023-05-22 22:00:58 -0700

    SHASUM update 0.0.1

--- Formula/awtest.rb
+++ Formula/awtest.rb
@@ -1,9 +1,9 @@
 class AWTest < Formula
   desc "A tool for pentesting found AWS credentials"
-  homepage "https://github.com/MillerMedia/AWTest"
-  url "https://github.com/MillerMedia/kneejerk/archive/refs/tags/v0.0.1.tar.gz"
+  homepage "https://github.com/MillerMedia/awtest"
+  url "https://github.com/MillerMedia/awtest/archive/refs/tags/v0.0.1.tar.gz"
   version "0.0.1"
-  sha256 "9206d7da24f70f46bc7bc330304b40a4715c82b4c0580a0c40340a95801fd34c"
+  sha256 "af224edd81bfbd10c54d0a73278a3b2730b9fafe27882cfabb5064f88f7daa0f"
 
   depends_on "go" => :build
 
