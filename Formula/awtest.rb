242fe8dc90652086dd7d3cef88af0e9a2df10269
Matt Miller <matt@miller-media.com>
2023-08-16 19:20:09 -0700

    Version 0.1.0

--- Formula/awtest.rb
+++ Formula/awtest.rb
@@ -1,9 +1,9 @@
 class Awtest < Formula
   desc "A tool for pentesting found AWS credentials"
   homepage "https://github.com/MillerMedia/awtest"
-  url "https://github.com/MillerMedia/awtest/archive/refs/tags/v0.0.1.tar.gz"
-  version "0.0.1"
-  sha256 "af224edd81bfbd10c54d0a73278a3b2730b9fafe27882cfabb5064f88f7daa0f"
+  url "https://github.com/MillerMedia/awtest/archive/refs/tags/v0.1.0.tar.gz"
+  version "0.1.0"
+  sha256 "74c7e461e556de786956508331450c05dda5841c9d6f7e5551f81bc06045d227"
 
   depends_on "go" => :build
 
